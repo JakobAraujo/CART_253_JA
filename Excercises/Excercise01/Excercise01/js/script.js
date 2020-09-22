e42950bf2e60edf43ccab0132214b79da5bcfb5f
JakobAraujo <55066442+JakobAraujo@users.noreply.github.com>
2020-09-21 21:45:38 -0400

    Ex01
    Finished.

--- Excercises/Excercise01/Excercise01/js/script.js
+++ Excercises/Excercise01/Excercise01/js/script.js
@@ -6,12 +6,12 @@ Jakob Araujo
 let cWid = 0;
 //Colours for Circle
 let r1 = 255;
-let g1 = 50;
+let g1 = 0;
 let b1 = 0;
 //Square X
 let sX = 1;
 // Colours for Square
-let rectShade = 255;
+let rectS = 0;
 //Colours for Triangle
 let r3 = 30;
 let g3 = 20;
@@ -22,28 +22,38 @@ let x1 = 80;
 
 function setup() {
   createCanvas(640, 640);
-  background(0);
-
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
-  //Connect Triangle to mouse
+  background(0);
+  //Connect Triangle Y postion and colour to mouse
   y1 = mouseY;
+  g3 = mouseY;
   r3 = mouseX;
-  //Connect the Circle's width to the mouse.
-  cWid = mouseX + mouseY;
+
   //Have the Squares X co-ordinate steadily increase.
-  sX ++;
-  //Map the shade of the square.
-  rectX = map(sX, 0, width, 20, 60);
+  sX = sX + random();
+  
+  //Map the x position of the square.
+  rectS = mouseX;
+  rectX = map(sX, 0, width, 20, 500);
+  rectShade = map (rectS, 1, 255, 25, 200);
+
+  //Connect the Circle's width to the mouse. And constrain it.
+  cWCon = constrain(mouseX, 1, 400);
+  cWid = cWCon + mouseY;
+
+  //Colours for the circle.
+  g1 = random(1, 100);
+  b1 = random(1, 100);
+  r1 = cWCon;
   //Ellipse 1
   fill(r1, g1, b1);
   stroke(255, 0, 0);
-  ellipse (mouseX, mouseY, cWid);
+  ellipse (mouseX, mouseY, cWCon);
 
   //Rectangle 1
   fill(rectShade);

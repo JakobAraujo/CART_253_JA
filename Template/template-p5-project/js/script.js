79daf4a842a3523d9ade32ea4a5f9f19465756a4
JakobAraujo <55066442+JakobAraujo@users.noreply.github.com>
2020-09-24 12:27:17 -0400

    Activity 04
    COVID works

--- Template/template-p5-project/js/script.js
+++ Template/template-p5-project/js/script.js
@@ -1,14 +1,19 @@
 /**************************************************
-Template p5 project
-Pippin Barr
-
-Here is a description of this template p5 project.
+Activiy 04
+Jakob Araujo
 **************************************************/
+
+
+//The Y position for COVID
+let covY = 0;
+//The X position for COVID
+let covX = 0;
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(640, 640);
 
 }
 
@@ -16,5 +21,22 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
+  //COVID:
+  //Steadily increase COVID'S X postion
+  covX ++;
+  //loops COVID across the screen
+  if(covX > width){
+    covX = 0;
+  }
+  //Randomize the Y position for COVID
+  if(covY > height){
+  covY = random(0, height);
+} else {covY ++}
+
+  fill(255, 0, 0);
+  ellipse(covX, covY, 64);
+
+  //Player
 
 }

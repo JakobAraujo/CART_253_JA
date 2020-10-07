7f6b6c2d9ecadfa9ee9fd54b414a63e3051a494c
JakobAraujo <55066442+JakobAraujo@users.noreply.github.com>
2020-10-07 18:45:01 -0400

    Activity 5
    Both circles drawn and moving

--- Activities/5/Activity05/js/script.js
+++ Activities/5/Activity05/js/script.js
@@ -23,16 +23,26 @@ let lover2={
 
 function setup() {
   createCanvas(1000, 1000);
-  lover1.x = width / 3;
-  lover1.y = height / 2;
 
-  lover2.x = width  / 6;
-  lover2.y = height / 4;
+  lover1.x = width / 2;
+  lover1.y = height / 2;
+  lover1.speed = random(-5, 5);
+
+  lover2.x = width  / 2;
+  lover2.y = height / 3;
+  lover2.speed = random(-5, 5);
+
+  console.log(lover2.speed);
+  console.log(lover1.speed);
 }
 
 function draw() {
   background(0);
 
+  lover1.yv = lover1.speed;
+  lover1.y = lover1.y + lover1.yv;
+  lover1.xv = lover1.speed;
+  lover1.x = lover1.x + lover1.xv;
   //Draw lover1
   push();
     ellipseMode(CENTER);
@@ -41,6 +51,10 @@ function draw() {
     ellipse(lover1.x, lover1.y, lover1.w);
   pop();
 
+  lover2.yv = lover2.speed;
+  lover2.y = lover2.y + lover2.yv;
+  lover2.xv = lover2.speed;
+  lover2.x = lover2.x + lover2.xv;
   //Draw lover2
   push();
     ellipseMode(CENTER);

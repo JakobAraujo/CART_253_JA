a0ccdc98be94193019c098766ade7c8765bec244
JakobAraujo <55066442+JakobAraujo@users.noreply.github.com>
2020-10-08 20:36:44 -0400

    Activity 5
    Finished

--- Activities/5/Activity05/js/script.js
+++ Activities/5/Activity05/js/script.js
@@ -2,13 +2,19 @@
 Activiy 05
 Jakob Araujo
 **************************************************/
+let state = `start`;
+let end = `You Die Alone`;
+let love = `<3Love Springs Eternal<3`;
+let start = `I Wanna Know What Love Is!`;
+
 let lover1={
   x: 0,
   y: 0,
   w: 69,
   xv: 0,
   yv: 0,
-  speed: 5
+  speedX: 0,
+  seedY: 0
 }
 
 let lover2={
@@ -17,59 +23,98 @@ let lover2={
   w: 69,
   xv: 0,
   yv: 0,
-  speed: 5
+  speedX: 0,
+  speedY: 0
 }
 
 
-function setup() {
-  createCanvas(1000, 1000);
-
+function startPositions1(){
   lover1.x = width / 2;
   lover1.y = height / 2;
-  lover1.speed = random(-5, 5);
-
+  lover1.speedX = random(-1, 1);
+  lover1.speedY = random(-1, 1);
+}
+function startPositions2(){
   lover2.x = width  / 2;
-  lover2.y = height / 3;
-  lover2.speed = random(-5, 5);
+  lover2.y = height / 4;
+  lover2.speedX = random(-1, 1);
+  lover2.speedY = random(-1, 1);
+}
 
-  console.log(lover2.speed);
-  console.log(lover1.speed);
+function mousePressed(){
+  if(state === `start`){
+    state = `simulation`;
+  }
+}
+
+function setup() {
+  createCanvas(1000, 500);
+  startPositions1();
+  startPositions2();
 }
 
 function draw() {
   background(0);
-
+  stateChange();
+  if(state === `simulation`){
   move1();
-  //Draw lover1
   display1();
 
   move2();
   //Draw lover2
   display2();
 
+  collision();
+  edgeDetect();
+}
 
 
+/////////////////////////////////////////////////
+  function stateChange(){
+    if(state === `start`){
+      titleScreen();
+    } else if (state === `end`) {
+      endScreen();
+    } else if (state === `love`){
+      loveScreen();
+    }
+  }
 
   function endScreen(){
-    textAlign();
-    textSize();
-    text();
+    fill(255);
+    textAlign(CENTER);
+    textSize(65);
+    text(end, width/2, height/2);
   }
+
+  function loveScreen(){
+    fill(255);
+    textAlign(CENTER);
+    textSize(65);
+    text(love, width/2, height/2);
+  }
+  function titleScreen(){
+    fill(255);
+    textAlign(CENTER);
+    textSize(65);
+    text(start, width/2, height/2);
+  }
+
   function edgeDetect(){
     //Screen wrap
     if(lover1.x > width) {
-      //text
+      state = `end`
     }
     if(lover1.x < 0){
-      //text
+      state = `end`
     }
 
     if(lover1.y > height){
-      //text
+      state = `end`
     }
 
     if(lover1.y < 0){
-      //text
+      state = `end`
     }
 
   }
@@ -92,22 +137,24 @@ function draw() {
   }
   function move1(){
     //Movement for Lover1
-    lover1.yv = lover1.speed;
+    lover1.yv = lover1.speedY;
     lover1.y = lover1.y + lover1.yv;
-    lover1.xv = lover1.speed;
+    lover1.xv = lover1.speedX;
     lover1.x = lover1.x + lover1.xv;
   }
   function move2(){
     //Moverment for Lover2
-    lover2.yv = lover2.speed;
+    lover2.yv = lover2.speedY;
     lover2.y = lover2.y + lover2.yv;
-    lover2.xv = lover2.speed;
+    lover2.xv = lover2.speedX;
     lover2.x = lover2.x + lover2.xv;
   }
   function collision(){
-    let d = dist(lover1.w, lover2.w);
-    if(d < lover1.w + lover2.w /2){
-      noLoop();
+    let d = dist(lover1.x, lover1.y, lover2.x, lover2.y);
+    if(d < lover1.w + lover2.w /25){
+      state = `love`;
     }
   }
+
+
 }

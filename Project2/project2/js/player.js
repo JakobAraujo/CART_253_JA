e086cd32c98808b4b9bc6b144bc4881ad267789f
JakobAraujo <55066442+JakobAraujo@users.noreply.github.com>
2020-11-09 17:37:47 -0500

    P2
    Completed player constructor

--- Project2/project2/js/player.js
+++ Project2/project2/js/player.js
@@ -4,7 +4,12 @@ Jakob Araujo
 **************************************************/
 
 class Player{
-  constructor(){
-    
+  constructor(x, y, v, size, speed){
+    this.x = x;
+    this.y = y;
+    this.v = v;
+    this.size = size;
+    this.speed = speed;
   }
+
 }
--- Project2/project2/js/script.js
+++ Project2/project2/js/script.js
@@ -2,10 +2,18 @@
 Project 2
 Jakob Araujo
 **************************************************/
-
+let player;
+let playerAt = {
+  x:,
+  y:,
+  v:,
+  size:,
+  speed:,
+}
 
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
+  player = new Player(playerAt.x, playerAt.y, playerAt.v, playerAt.size, playerAt.speed);
 }
 
 function draw() {
